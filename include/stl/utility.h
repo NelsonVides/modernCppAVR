ccad72147752a946db4c5929ed315de8ed49e386
Nelson Vides <videsnelson@gmail.com>
2018-04-08 21:38:39 +0200

    stl::forward added to utilities, as per gcc

--- include/stl/utility.h
+++ include/stl/utility.h
@@ -12,7 +12,46 @@ decltype(auto) move(T&& param)
     return static_cast<ReturnType>(param);
 }
 
+/// identity
+namespace detail {
+    template<typename T>
+    struct identity
+    {
+        using type = T;
+    };
 }
+
+/// forward (as per N2835)
+/// Forward lvalues as rvalues.
+template<typename T>
+inline constexpr typename enable_if<!is_lvalue_reference<T>::value, T&&>::type
+forward(typename detail::identity<T>::type& t)
+{
+    return static_cast<T&&>(t); 
+}
+
+/// Forward rvalues as rvalues.
+template<typename T>
+inline constexpr typename enable_if<!is_lvalue_reference<T>::value, T&&>::type
+forward(typename detail::identity<T>::type&& t)
+{ 
+    return static_cast<T&&>(t); 
+}
+
+/// Forward lvalues as lvalues.
+template<typename T>
+inline constexpr typename enable_if<is_lvalue_reference<T>::value, T>::type
+forward(typename detail::identity<T>::type t)
+{ 
+    return t;
+}
+
+/// Prevent forwarding rvalues as const lvalues.
+template<typename T>
+inline constexpr typename enable_if<is_lvalue_reference<T>::value, T>::type
+forward(remove_reference_t<T>&& t) = delete;
+
+} /* end of namespace stl */
 
 #endif
 
