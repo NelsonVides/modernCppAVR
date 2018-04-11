02bb9c2d7adcfa02763e844d29979a78d0700ffd
Nelson Vides <videsnelson@gmail.com>
2018-04-11 11:06:20 +0200

    added the mega328p header file, with some edits to main.cpp and makefile, to test if all the interface ported is working

--- main.cpp
+++ main.cpp
@@ -5,22 +5,17 @@
 #include <stl/type_traits.h>
 #include <stl/utility.h>
 #include <avr/io.h>
+#include "mega328p.h"
 
-
-class Point {
-public:
-    Point() : x(0), y(0) {};
-    Point(int a, int b) : x(a), y(b) {};
-private:
-    int x;
-    int y;
-};
-
-int main(void)
+int main()
 {
-    Point a;
-    Point b;
-    Point c = static_cast<Point>(b);
-    c = stl::move(a);
-    while(1);
+    //if(vAVR::mcu::portb::port<3>::get())
+    //{
+        vAVR::mcu::portb::port<7>::set();
+    //}
+    //else
+    //{
+        vAVR::mcu::portb::port<7>::get();
+    //}
 }
+
--- makefile
+++ makefile
@@ -8,9 +8,9 @@
 #
 
 CC = avr-g++
-CPPFLAGS = -Wall -Wextra -Werror -Woverflow -std=c++14 -fno-exceptions
+CPPFLAGS = -Wall -Wextra -Werror -Woverflow -std=c++14 -fno-exceptions -Wno-int-to-pointer-cast
 AVRFLAGS = -mmcu=atmega2560
-INCLUDES = -I ./include
+INCLUDES = -I ./include mega328p.h
 SOURCES = main.cpp
 OUTDIR = bin
 OBJ_DIR := $(SOURCES:%.cpp=bin/%)
